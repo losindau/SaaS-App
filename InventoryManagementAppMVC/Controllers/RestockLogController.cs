7028cbb3768cb8ee4165066f3b8c5016f6ff6d16
Lồ Sìn Dậu <89368649+dau12345@users.noreply.github.com>
2023-05-14 22:16:47 +0700

    Create restocklog

--- InventoryManagementAppMVC/Controllers/RestockLogController.cs
+++ InventoryManagementAppMVC/Controllers/RestockLogController.cs
@@ -7,6 +7,7 @@ using System.Text.Json.Serialization;
 using System.Text.Json;
 using InventoryManagementAppMVC.ViewModels;
 using InventoryManagementAppMVC.Helper;
+using InventoryManagementAppMVC.Enum;
 
 namespace InventoryManagementAppMVC.Controllers
 {
@@ -45,37 +46,152 @@ namespace InventoryManagementAppMVC.Controllers
             return View(responsePage);
         }
 
-        [HttpPost]
+        [HttpGet]
         public async Task<IActionResult> Create()
         {
-            RestockLogVM restocklogVM = new RestockLogVM();
-
-            var companyID = _httpContextAccessor.HttpContext?.User.GetUserCompanyID();
-
+            TruckVM truckVM = new TruckVM();
             var accessToken = await HttpContext.GetTokenAsync("access_token");
 
             _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
 
-
-            restocklogVM.RequestDate = DateTime.Now;
-            restocklogVM.LogState = Enum.LogState.Pending;
-            restocklogVM.TruckID = 3;
-            restocklogVM.LicensePlate = "ABD-123";
-            restocklogVM.AppUserID = _httpContextAccessor.HttpContext?.User.GetUserId();
-            restocklogVM.AppUserName = _httpContextAccessor.HttpContext?.User.GetUserName();
-            restocklogVM.CompanyID = int.Parse(companyID);
-            restocklogVM.isDeleted = false;
-
-            var responsePost = await _httpClient.PostAsJsonAsync("api/RestockLog", restocklogVM);
-
-            if (!responsePost.IsSuccessStatusCode)
+            var truckID = _httpContextAccessor.HttpContext?.User.GetUserTruckID();
+            var response = await _httpClient.GetAsync("api/Truck/" + truckID);
+            if (response.IsSuccessStatusCode)
             {
-                TempData["Error"] = await responsePost.Content.ReadAsStringAsync();
-                return RedirectToAction("Index", new { page = 1 });
+                var apiResponse = await response.Content.ReadAsStreamAsync();
+                truckVM = await JsonSerializer.DeserializeAsync<TruckVM>(apiResponse, new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true,
+                    Converters = { new JsonStringEnumConverter() },
+                    DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+                });
+            }
+
+            CreateRestockLogVM createRestockLogVM = new CreateRestockLogVM()
+            {
+                TruckID = truckVM.TruckID,
+                LicensePlate = truckVM.LicensePlate,
+                TruckStockItems = truckVM.TruckStockItems.ToList()
+            };
+
+            foreach (var item in truckVM.TruckStockItems)
+            {
+                createRestockLogVM.StockItemNames.Add((int)item.StockItemID, item.StockItem.Name);
+                createRestockLogVM.StockItemQuantities.Add((int)item.StockItemID, 0);
+            }
+
+            return View(createRestockLogVM);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateRestockLogVM createRestockLogVM)
+        {
+            int count = 0;
+            foreach (var item in createRestockLogVM.StockItemQuantities)
+            {
+                if (item.Value > 0)
+                {
+                    count++;
+                }
+            }
+
+            if (count < 1)
+            {
+                TempData["Error"] = "To create restocklog you must change quantity of at least 1 item";
+                return RedirectToAction("MyRestockLog", new { page = 1 });
+            }
+
+            //Create master restocklog
+            var userID = _httpContextAccessor.HttpContext?.User.GetUserId();
+            var userName = _httpContextAccessor.HttpContext?.User.GetUserName();
+            var companyID = _httpContextAccessor.HttpContext?.User.GetUserCompanyID();
+
+            RestockLogVM restocklogVM = new RestockLogVM()
+            {
+                RequestDate = DateTime.Now,
+                LogState = LogState.Pending,
+                RestockState = RestockState.NeedAccept,
+                TruckID = createRestockLogVM.TruckID,
+                LicensePlate = createRestockLogVM.LicensePlate,
+                AppUserID = userID,
+                AppUserName = userName,
+                CompanyID = int.Parse(companyID),
+                isDeleted = false
+            };
+
+            //Post master restocklog then receive master restocklog id
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+            var responsePostRestockLog = await _httpClient.PostAsJsonAsync("api/RestockLog", restocklogVM);
+            var postRestockLogContent = await responsePostRestockLog.Content.ReadAsStringAsync();
+
+            if (!responsePostRestockLog.IsSuccessStatusCode)
+            {
+                TempData["Error"] = postRestockLogContent;
+                return RedirectToAction("MyRestockLog", new { page = 1 });
+            }
+
+            //Create detail usagelogs
+            List<DetailRestockLogVM> detailRestockLogVMs = new List<DetailRestockLogVM>();
+
+            foreach (var item in createRestockLogVM.StockItemQuantities)
+            {
+                int stockItemID = item.Key;
+                int quantity = item.Value;
+
+                if (quantity > 0)
+                {
+                    DetailRestockLogVM newDetailRestockLog = new DetailRestockLogVM()
+                    {
+                        StockItemID = stockItemID,
+                        StockItemName = createRestockLogVM.StockItemNames[stockItemID],
+                        Quantity = quantity,
+                        RestockLogID = int.Parse(postRestockLogContent),
+                        CompanyID = int.Parse(companyID),
+                        isDeleted = false
+                    };
+
+                    detailRestockLogVMs.Add(newDetailRestockLog);
+                }
+            }
+
+            //Post list of detail restocklogs
+            var responsePostDetailRestockLog = await _httpClient.PostAsJsonAsync("api/DetailRestockLog", detailRestockLogVMs);
+            var postDetailRestockLogContent = await responsePostDetailRestockLog.Content.ReadAsStringAsync();
+
+            if (!responsePostDetailRestockLog.IsSuccessStatusCode)
+            {
+                TempData["Error"] = postDetailRestockLogContent;
+                return RedirectToAction("MyRestockLog", new { page = 1 });
             }
 
             TempData["Success"] = "Create new restock log successfully";
-            return RedirectToAction("Index", new { page = 1 });
+            return RedirectToAction("MyRestockLog", new { page = 1 });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> MyRestockLog(int page)
+        {
+            ResponsePagination responsePage = new ResponsePagination();
+
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+            var userID = _httpContextAccessor.HttpContext?.User.GetUserId();
+            var response = await _httpClient.GetAsync("api/RestockLog/" + page + "/myrestocklogs/" + userID);
+            if (response.IsSuccessStatusCode)
+            {
+                var apiResponse = await response.Content.ReadAsStreamAsync();
+                responsePage = await JsonSerializer.DeserializeAsync<ResponsePagination>(apiResponse, new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true,
+                    Converters = { new JsonStringEnumConverter() },
+                    DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+                });
+            }
+
+            return View(responsePage);
         }
     }
 }
---
+++ InventoryManagementAppMVC/ViewModels/CreateRestockLogVM.cs
@@ -0,0 +1,11 @@
+﻿namespace InventoryManagementAppMVC.ViewModels
+{
+    public class CreateRestockLogVM
+    {
+        public int? TruckID { get; set; }
+        public string LicensePlate { get; set; }
+        public List<TruckStockItemVM> TruckStockItems { get; set; }
+        public Dictionary<int, int> StockItemQuantities { get; set; } = new Dictionary<int, int>();
+        public Dictionary<int, string> StockItemNames { get; set; } = new Dictionary<int, string>();
+    }
+}
