a3d67545733a256bea7124647edb0d8e240d2aba
Lồ Sìn Dậu <89368649+dau12345@users.noreply.github.com>
2023-05-14 18:36:18 +0700

    Create usagelog
    Tạo usagelog rồi tạo list detailusagelog rồi trừ số lượng của truckstockitem

--- InventoryManagementAppMVC/Controllers/UsageLogController.cs
+++ InventoryManagementAppMVC/Controllers/UsageLogController.cs
@@ -7,6 +7,7 @@ using System.Text.Json.Serialization;
 using System.Text.Json;
 using InventoryManagementAppMVC.ViewModels;
 using InventoryManagementAppMVC.Helper;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace InventoryManagementAppMVC.Controllers
 {
@@ -27,7 +28,6 @@ namespace InventoryManagementAppMVC.Controllers
             ResponsePagination responsePage = new ResponsePagination();
 
             var accessToken = await HttpContext.GetTokenAsync("access_token");
-
             _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
 
             var response = await _httpClient.GetAsync("api/UsageLog/" + page + "/usagelogs");
@@ -45,36 +45,168 @@ namespace InventoryManagementAppMVC.Controllers
             return View(responsePage);
         }
 
-        [HttpPost]
+        [HttpGet]
         public async Task<IActionResult> Create()
         {
-            UsageLogVM usagelogVM = new UsageLogVM();
-
-            var companyID = _httpContextAccessor.HttpContext?.User.GetUserCompanyID();
-
+            AppUserVM appUserVM = new AppUserVM();
             var accessToken = await HttpContext.GetTokenAsync("access_token");
 
             _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
 
-
-            usagelogVM.Date = DateTime.Now;
-            usagelogVM.TruckID = 3;
-            usagelogVM.LicensePlate = "ABD-123";
-            usagelogVM.AppUserID = _httpContextAccessor.HttpContext?.User.GetUserId();
-            usagelogVM.AppUserName = _httpContextAccessor.HttpContext?.User.GetUserName();
-            usagelogVM.CompanyID = int.Parse(companyID);
-            usagelogVM.isDeleted = false;
-
-            var responsePost = await _httpClient.PostAsJsonAsync("api/UsageLog", usagelogVM);
-
-            if (!responsePost.IsSuccessStatusCode)
+            var userID = _httpContextAccessor.HttpContext?.User.GetUserId();
+            var response = await _httpClient.GetAsync("api/Account/" + userID);
+            if (response.IsSuccessStatusCode)
             {
-                TempData["Error"] = await responsePost.Content.ReadAsStringAsync();
-                return RedirectToAction("Index", new { page = 1 });
+                var apiResponse = await response.Content.ReadAsStreamAsync();
+                appUserVM = await JsonSerializer.DeserializeAsync<AppUserVM>(apiResponse, new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true,
+                    Converters = { new JsonStringEnumConverter() },
+                    DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+                });
+            }
+
+            CreateUsageLogVM createUsageLogVM = new CreateUsageLogVM()
+            {
+                TruckID = appUserVM.TruckID,
+                LicensePlate = appUserVM.Truck.LicensePlate,
+                TruckStockItems = appUserVM.Truck.TruckStockItems.ToList()
+            };
+
+            foreach (var item in appUserVM.Truck.TruckStockItems)
+            {
+                createUsageLogVM.StockItemNames.Add((int)item.StockItemID, item.StockItem.Name);
+                createUsageLogVM.StockItemQuantities.Add((int)item.StockItemID, 0);
+                createUsageLogVM.TruckStockItemIDs.Add((int)item.StockItemID, item.TruckStockItemID);
+            }
+
+            return View(createUsageLogVM);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateUsageLogVM createUsageLogVM)
+        {
+            // Create master usagelog
+            var userID = _httpContextAccessor.HttpContext?.User.GetUserId();
+            var userName = _httpContextAccessor.HttpContext?.User.GetUserName();
+            var companyID = _httpContextAccessor.HttpContext?.User.GetUserCompanyID();
+
+            UsageLogVM usagelogVM = new UsageLogVM()
+            {
+                Date = DateTime.Now,
+                TruckID = createUsageLogVM.TruckID,
+                LicensePlate = createUsageLogVM.LicensePlate,
+                AppUserID = userID,
+                AppUserName = userName,
+                CompanyID = int.Parse(companyID),
+                isDeleted = false
+            };
+            
+            //Post master usagelog then receive master usagelog id
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+            var responsePostUsageLog = await _httpClient.PostAsJsonAsync("api/UsageLog", usagelogVM);
+            var postUsageLogContent = await responsePostUsageLog.Content.ReadAsStringAsync();
+
+            if (!responsePostUsageLog.IsSuccessStatusCode)
+            {
+                TempData["Error"] = postUsageLogContent;
+                return RedirectToAction("MyUsageLog", new { page = 1 });
+            }
+
+            //Create detail usagelogs
+            List<DetailUsageLogVM> detailUsageLogVMs = new List<DetailUsageLogVM>();
+
+            foreach (var item in createUsageLogVM.StockItemQuantities)
+            {
+                int stockItemID = item.Key;
+                int quantity = item.Value;
+
+                if (quantity > 0)
+                {
+                    DetailUsageLogVM newDetailUsageLog = new DetailUsageLogVM()
+                    {
+                        StockItemID = stockItemID,
+                        StockItemName = createUsageLogVM.StockItemNames[stockItemID],
+                        Quantity = quantity,
+                        UsageLogID = int.Parse(postUsageLogContent),
+                        CompanyID = int.Parse(companyID),
+                        isDeleted = false
+                    };
+
+                    detailUsageLogVMs.Add(newDetailUsageLog);
+
+                    //Get truck stock item
+                    var truckStockItemID = createUsageLogVM.TruckStockItemIDs[stockItemID];
+                    TruckStockItemVM responseTruckStockItemVM = new TruckStockItemVM();
+
+                    var response = await _httpClient.GetAsync("api/TruckStockItem/" + truckStockItemID);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var apiResponse = await response.Content.ReadAsStreamAsync();
+                        responseTruckStockItemVM = await JsonSerializer.DeserializeAsync<TruckStockItemVM>(apiResponse, new JsonSerializerOptions
+                        {
+                            PropertyNameCaseInsensitive = true,
+                            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+                        });
+                    }
+                    responseTruckStockItemVM.QuantityInTruck -= quantity;
+
+                    //Put truck stock item to update quantity in truck
+                    var responsePutTruckStockItem = await _httpClient.PutAsJsonAsync("api/TruckStockItem/" + truckStockItemID, responseTruckStockItemVM);
+                    var putTruckStockItemContent = await responsePutTruckStockItem.Content.ReadAsStringAsync();
+
+                    if (!responsePutTruckStockItem.IsSuccessStatusCode)
+                    {
+                        TempData["Error"] = putTruckStockItemContent;
+                        return RedirectToAction("MyUsageLog", new { page = 1 });
+                    }
+                }
+            }
+
+            if (detailUsageLogVMs.Count() < 1)
+            {
+                TempData["Error"] = "To create usagelog you must change quantity of at least 1 item";
+                return View();
+            }
+
+            //Post list of detail usagelogs
+            var responsePostDetailUsageLog = await _httpClient.PostAsJsonAsync("api/DetailUsageLog", detailUsageLogVMs);
+            var postDetailUsageLogContent = await responsePostDetailUsageLog.Content.ReadAsStringAsync();
+
+            if (!responsePostDetailUsageLog.IsSuccessStatusCode)
+            {
+                TempData["Error"] = postDetailUsageLogContent;
+                return RedirectToAction("MyUsageLog", new { page = 1 });
             }
 
             TempData["Success"] = "Create new usage log successfully";
-            return RedirectToAction("Index", new { page = 1 });
+            return RedirectToAction("MyUsageLog", new { page = 1 });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> MyUsageLog(int page)
+        {
+            ResponsePagination responsePage = new ResponsePagination();
+
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+            var userID = _httpContextAccessor.HttpContext?.User.GetUserId();
+            var response = await _httpClient.GetAsync("api/UsageLog/" + page + "/myusagelogs/" + userID);
+            if (response.IsSuccessStatusCode)
+            {
+                var apiResponse = await response.Content.ReadAsStreamAsync();
+                responsePage = await JsonSerializer.DeserializeAsync<ResponsePagination>(apiResponse, new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true,
+                    Converters = { new JsonStringEnumConverter() },
+                    DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+                });
+            }
+
+            return View(responsePage);
         }
     }
 }
